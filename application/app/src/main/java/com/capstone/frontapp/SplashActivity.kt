62a182c834d4ad01ce1c9ccdccf881f4f97bb8b5
parkhyeongmo <85858435+parkhyeongmo@users.noreply.github.com>
2022-10-07 22:29:54 +0900

    etc.

--- application/app/src/main/java/com/capstone/frontapp/SplashActivity.kt
+++ application/app/src/main/java/com/capstone/frontapp/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.capstone.frontapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+
+
     }
+
 }
