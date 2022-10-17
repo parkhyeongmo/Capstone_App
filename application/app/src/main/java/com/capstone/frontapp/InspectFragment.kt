2ef320d292ceb4c6511eb7cc68c568a6d5ba9dd9
parkhyeongmo <85858435+parkhyeongmo@users.noreply.github.com>
2022-10-17 23:31:39 +0900

    Add Load Image
    갤러리, 카메라에서 사진 호출 후 ImageView에 출력 기능 추가

--- application/app/src/main/java/com/capstone/frontapp/InspectFragment.kt
+++ application/app/src/main/java/com/capstone/frontapp/InspectFragment.kt
@@ -1,20 +1,30 @@
 package com.capstone.frontapp
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import java.net.URI
 import java.util.jar.Manifest
 
+private const val PICK_FROM_ALBUM = 0
 
 class InspectFragment : Fragment() {
 
@@ -28,15 +38,21 @@ class InspectFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_inspect, container, false)
 
+
         // 계정 관리 화면 이동
         view.findViewById<Button>(R.id.btn_account).setOnClickListener {
             val intent = Intent(context, AccountActivity::class.java)
             startActivity(intent)
         }
 
-        view.findViewById<ImageView>(R.id.image_upload).setOnClickListener {
+        // 갤러리 사진 선택 버튼
+        view.findViewById<Button>(R.id.btn_gallery).setOnClickListener {
+            (activity as UserActivity).selectPhoto()
+        }
 
-
+        // 카메라 사진 촬영 버튼
+        view.findViewById<Button>(R.id.btn_shoot).setOnClickListener {
+            (activity as UserActivity).shootPhoto()
         }
 
         // 사용자 검사 내역 화면 이동
--- application/app/src/main/java/com/capstone/frontapp/UserActivity.kt
+++ application/app/src/main/java/com/capstone/frontapp/UserActivity.kt
@@ -1,10 +1,28 @@
 package com.capstone.frontapp
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
 import java.util.jar.Manifest
 
 class UserActivity : AppCompatActivity() {
@@ -25,10 +43,6 @@ class UserActivity : AppCompatActivity() {
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
         android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
-//    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-//        val image = findViewById<ImageView>(R.id.image_upload)
-//        image.load(uri)
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,4 +51,56 @@ class UserActivity : AppCompatActivity() {
         // 권한 요청 실행
         requestMultiplePermission.launch(permissionList)
     }
+
+    // 갤러리에서 사진 선택
+    fun selectPhoto() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+    // 카메라로 사진 촬영
+    fun shootPhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 3000)
+    }
+
+    // 갤러리에서 선택된 사진 경로 저장 후 이미지뷰에 출력
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            Toast.makeText(this, "잘못된 접근입니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        when(requestCode) {
+            // 갤러리에서 사진 호출 시
+            2000 -> {
+                val selectedImageURI : Uri? = data?.data
+
+                if(selectedImageURI != null) {
+                    val imagePreview = findViewById<ImageView>(R.id.imagePreview)
+                    imagePreview.setImageURI(selectedImageURI)
+                    Log.i("성공", "${selectedImageURI}")
+
+                }
+                else {
+                    Toast.makeText(this, "사진 로드 실패", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            // 카메라 사진 촬영 시
+            3000 -> {
+                val bitmap = data?.extras?.get("data") as Bitmap
+                val imagePreview = findViewById<ImageView>(R.id.imagePreview)
+                imagePreview.setImageBitmap(bitmap)
+            }
+
+            else -> {
+                Toast.makeText(this, "잘못된 접근입니다", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
